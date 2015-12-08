fcd63472ccb3bcafa06f791c4f9f57b46841f7f2
unknown <courses@gregiscool.com>
2015-12-08 00:45:40 -0800

    Updated with more test code to have it rotate correctly when the mouse moves

--- Assets/Scripts/ActionInspect.cs
+++ Assets/Scripts/ActionInspect.cs
@@ -1,9 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
 
 public class ActionInspect : MonoBehaviour {
 
     Transform originalTransform;
+    GameObject inspectCamera;
 
 	// Use this for initialization
 	void Start () {
@@ -12,8 +14,24 @@ public class ActionInspect : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+        GetInput();
 	}
+
+    private void GetInput() {
+        float xRotation = CrossPlatformInputManager.GetAxis("Mouse X");
+        float yRotation = CrossPlatformInputManager.GetAxis("Mouse Y");
+
+
+
+        //this.transform.Rotate(new Vector3(5, 0, 0));
+
+        this.transform.RotateAround(inspectCamera.transform.up, -xRotation);
+        this.transform.RotateAround(inspectCamera.transform.right, yRotation);
+
+        //this.transform.RotateAround(Camera.main.transform.up, -Mathf.Deg2Rad * yRotation * 10);
+        //this.transform.RotateAround(Camera.main.transform.right, Mathf.Deg2Rad * xRotation * 10);
+
+    }
 
     void Clicked(string option) {
         Debug.Log("ActionInspect clicked!");
@@ -22,20 +40,17 @@ public class ActionInspect : MonoBehaviour {
         originalTransform = this.transform;
 
         // find inspect camera
-        GameObject camera = GameObject.Find("InspectCamera");
+        this.inspectCamera = GameObject.Find("InspectCamera");
 
-        if (camera) {
+        if (inspectCamera) {
 
             // move or duplicate object to camera
-            this.transform.position = camera.transform.position;
+            this.transform.position = inspectCamera.transform.position;
 
-            this.transform.position += camera.transform.forward * 3.0f;
+            this.transform.position += inspectCamera.transform.forward * 3.0f;
 
             
             // enable inspect canvas and camera
         }
-
-
-
     }
 }
