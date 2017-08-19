8d5c5a86d52a98bf58b056b865996bb9327f575d
badical <c.stephen.palacio@gmail.com>
2017-08-19 14:48:10 -0700

    Created Key script for sound effect when picking up key
    - copied over from Door script

---
+++ Assets/Scripts/Key.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class Key : MonoBehaviour, IExamineHandler {
+
+
+    public AudioClip getKeySound;
+
+    // Use this for initialization
+    void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+    }
+
+    public void OnExamine(PointerEventData eventData) {
+       PlayAudio(this.getKeySound);
+    }
+
+    private void PlayAudio(AudioClip audioClip) {
+        if (audioClip != null) {
+            AudioSource.PlayClipAtPoint(audioClip, this.transform.position);
+        }
+    }
+}
